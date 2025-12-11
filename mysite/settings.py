75a9ec6150f2e6939d38ae1848296b914ce66126
JUANMAXD34 <juanguzmango01@gmail.com>
2025-12-10 23:26:22 -0600

    proyecto2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wu8+9=ey$c&7vn5ccqq%vsv8o(=7@z3*j06r8f*%!gvu$6rbzp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
